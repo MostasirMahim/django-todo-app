a8ca406bee91341aaf1c2e6398cc2aae82ab7b3d
Mostasir Mahim <mahimlive@gmail.com>
2025-10-13 02:03:21 +0600

    L3: POST opeartion. added data to database.

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
+
+
+def addTask(request):
+    print(request.POST['task'])
+    task = request.POST['task']
+    Todo.objects.create(title=task)
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from todo.models import Todo
 def home(request):
-    incomplete_task = Todo.objects.filter(is_completed=False)
+    incomplete_task = Todo.objects.filter(is_completed=False).order_by('-created_at')
     completed_task = Todo.objects.filter(is_completed=True)
     print(incomplete_task)
     context = {
