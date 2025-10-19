f5685d2f6cbbe97df92f19819166c9617cb9ce54
Mostasir Mahim <mahimlive@gmail.com>
2025-10-19 06:37:12 +0600

    test : undo a task.

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('markComplete/<int:todo_id>/', views.markComplete, name='markComplete'),
+    path('markUnComplete/<int:todo_id>/', views.markUnComplete, name='markUnComplete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -10,3 +10,15 @@ def addTask(request):
     task = request.POST['task']
     Todo.objects.create(title=task)
     return redirect('home')
+
+def markComplete(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.is_completed = True
+    todo.save()
+    return redirect('home')
+
+def markUnComplete(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.is_completed = False
+    todo.save()
+    return redirect('home')
